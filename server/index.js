58da6c9e11f2167bcbd67a9ce5d3e76c03fb6f9a
Brady Camp <bradyjcamp@gmail.com>
2022-04-06 21:04:05 -0400

    initial commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const caps = io.of('/caps');
 // const eventHandler = require('../lib/event-logger.js');
 
 
+
 function eventHandler(event, payload){
   let time = new Date();
   console.log('EVENT', {
@@ -15,16 +16,24 @@ function eventHandler(event, payload){
   });
 }
 
-//---io.on
+
 caps.on('connection', socket => {
   
   console.log('Socket connected!!!', socket.id);
   socket.emit('server-connect', { id: socket.id });
 
-  // this is room joining
-  socket.on('join', (payload) => {
-    socket.join(payload.roomId);
-    socket.emit('join', payload.roomId);
+ 
+  socket.on('join', ({ queueId }) => {
+    socket.join(queueId);
+    socket.emit('join', queueId);
+  });
+
+  socket.on('RECEIVED', (payload) => {
+    caps.emit('RECEIVED', payload);
+  });
+
+  socket.on('GETALL', (payload) => {
+    caps.emit('GETALL', payload);
   });
 
   socket.on('PICKUP', (payload) => {
@@ -42,10 +51,5 @@ caps.on('connection', socket => {
     caps.to(payload.vendorId).emit('DELIVERED', payload);
   });
 
-  // socket.on('PICKUP', (payload) => eventHandler(payload, 'PICKUP'));
-  // socket.on('PICKUP', driverPickupHandler);
-  // socket.on('IN-TRANSIT', (payload) => eventHandler(payload, 'IN-TRANSIT') );
-  // socket.on('DELIVERED', (payload) => eventHandler(payload, 'DELIVERED'));
-  // socket.on('DELIVERED', vendorDelivered);
 });
 
